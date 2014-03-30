a46ebee196d26f07ab9857c39e0fdcbe2bb55b06
Drew French <RectangleTangle@gmail.com>
2014-03-30 16:30:00 -0700

    Update jsave.js

--- jsave.js
+++ jsave.js
@@ -1,9 +1,9 @@
 /**
  * This library acts as a persistence abstraction layer for client-side JavaScript. This allows for the persistent
- * storage of JSON compatible objects using HTML5's `localStorage` or cookies with a single interface. This facilitates
- * robust client-side storage, using `localStorage` as the preferred mechanism while utilizing cookies as a fallback.
+ * storage of JSON compatible objects using HTML5's `localStorage` or cookies, using a single interface. This
+ * facilitates robust client-side storage, using `localStorage` as the preferred mechanism while utilizing cookies as a
+ * fallback.
  */
-
 
 jSave = {};
 
